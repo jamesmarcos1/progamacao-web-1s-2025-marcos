b3544cc8b350b8b6ca56ebcc4cec7530a29d1fdc
jamesmarcos1 <marcosandrebarrosmeneses@gmail.com>
2025-03-01 16:48:28 -0300

    Atividade 01 - Variáveis e tipos primitivos / e arquivo referente a aula 1 usando variaveis.

---
+++ Web-Aula01/variaveis.js
@@ -0,0 +1,39 @@
+// Variáveis em JavaScript
+var variavelVar = "Essa é uma variável var";
+let variavelLet = "Essa é uma variável let";
+const variavelConst = "Essa é uma variável const";
+
+// Tipos de variáveis
+// Number
+let numeroInteiro = 42;
+let numeroDecimal = 3.14;
+let numeroNegativo = -7;
+let numeroGrande = 1e6; // 1 milhão
+
+// String
+let stringSimples = 'Olá, mundo!';
+let stringDupla = "Bem-vindo ao JavaScript";
+let stringTemplate = `A soma de 2 e 3 é ${2 + 3}`;
+
+// Boolean
+let booleanVerdadeiro = true;
+let booleanFalso = false;
+
+// Outras variáveis
+let variavelNula = null;
+let variavelIndefinida;
+
+console.log(variavelVar);
+console.log(variavelLet);
+console.log(variavelConst);
+console.log(numeroInteiro);
+console.log(numeroDecimal);
+console.log(numeroNegativo);
+console.log(numeroGrande);
+console.log(stringSimples);
+console.log(stringDupla);
+console.log(stringTemplate);
+console.log(booleanVerdadeiro);
+console.log(booleanFalso);
+console.log(variavelNula);
+console.log(variavelIndefinida);
---
+++ atividade1/variaveisOperadores.js
@@ -0,0 +1,25 @@
+//Marcos André Barros Meneses - UC23100694
+
+// Operação de Soma
+const numA = 10;
+const numB = 5;
+const soma = numA + numB;
+console.log(`Resultado da soma entre ${numA} e ${numB} é ${soma}`);
+
+// Operação de Subtração
+const numC = 20;
+const numD = 8;
+const subtracao = numC - numD;
+console.log(`Resultado da subtração entre ${numC} e ${numD} é ${subtracao}`);
+
+// Operação de Multiplicação
+const numE = 4;
+const numF = 7;
+const multiplicacao = numE * numF;
+console.log(`Resultado da multiplicação entre ${numE} e ${numF} é ${multiplicacao}`);
+
+// Operação de Divisão
+const numG = 16;
+const numH = 2;
+const divisao = numG / numH;
+console.log(`Resultado da divisão entre ${numG} e ${numH} é ${divisao}`);
